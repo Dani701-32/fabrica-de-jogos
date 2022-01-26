7bff0d99520a5c490c35485f2b98949469c50e7b
Anderson Ribeiro Lopes <zeander21@outlook.com.br>
2022-01-26 11:20:41 -0300

    New universal web route for react app

--- routes/web.php
+++ routes/web.php
@@ -12,32 +12,7 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('index');
-});
-Route::get('/wordsearch/{slug}', function () {
-    return view('index');
-});
-Route::get('/quiz/{slug}', function () {
-    return view('index');
-});
-Route::get('/anagram/{slug}', function () {
-    return view('index');
-});
-Route::get('/create/quiz', function () {
-    return view('index');
-});
-Route::get('/create/anagram', function () {
-    return view('index');
-});
-Route::get('/create/wordsearch', function () {
-    return view('index');
-});
-Route::get('/create/trueorfalse', function () {
-    return view('index');
-});
-Route::get('/create/matchup', function () {
+Route::fallback(function() {
     return view('index');
 });
 
