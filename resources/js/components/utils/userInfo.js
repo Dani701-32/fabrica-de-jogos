c2be0e404c575f64f1de2897768d04cf2b870ef1
Anderson Ribeiro Lopes <zeander21@outlook.com.br>
2022-02-21 08:11:21 -0300

    Restricted message listener

--- resources/js/components/utils/userInfo.js
+++ resources/js/components/utils/userInfo.js
@@ -2,19 +2,21 @@ import axios from 'axios';
 
 export default function userInfo() {
     window.addEventListener('message', (event) => {
-        const api_address = event.data.api_address;
-        const token = event.data.user_token;
-        const config = {
-            headers: {
-                Authorization: `Bearer ${token}`
-            }
-        };
-        axios
-            .get(`https://${api_address}/api/validate`, config)
-            .then((response) => {
-                if (response.status === 200) {
-                    return { token: token, api_address: api_address };
+        if (event.origin !== window.location.origin) {
+            const api_address = event.data.api_address;
+            const token = event.data.user_token;
+            const config = {
+                headers: {
+                    Authorization: `Bearer ${token}`
                 }
-            });
+            };
+            axios
+                .get(`https://${api_address}/api/validate`, config)
+                .then((response) => {
+                    if (response.status === 200) {
+                        return { token: token, api_address: api_address };
+                    }
+                });
+        }
     });
 }
