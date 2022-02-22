80114f18095aa83595c4b54da23c243562a7fd43
Anderson Ribeiro Lopes <zeander21@outlook.com.br>
2022-02-22 09:48:08 -0300

    Fixed accept="image" prop

--- resources/js/components/layout/imageEditor.js
+++ resources/js/components/layout/imageEditor.js
@@ -110,7 +110,9 @@ export default function ImageEditor(props) {
                     <CardActions>
                         <Input
                             type="file"
-                            accept="image/*"
+                            inputProps={{
+                                accept: 'image/*'
+                            }}
                             onChange={onSelectFile}
                             required
                         />
