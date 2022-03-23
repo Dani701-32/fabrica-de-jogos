bea790bbb0fcfb8e1859b492db7da2c06c3ddfda
Anderson Ribeiro Lopes <zeander21@outlook.com.br>
2022-03-23 08:50:16 -0300

    Fix in base state

--- resources/js/store/reducers/baseReducer.js
+++ resources/js/store/reducers/baseReducer.js
@@ -5,12 +5,14 @@ const initialState = {
     token: localStorage.getItem('token'),
     api_address: localStorage.getItem('api_address'),
     origin: localStorage.getItem('origin'),
-    series: localStorage.getItem('series')
-        ? JSON.parse(localStorage.getItem('series'))
-        : { '': 0 },
-    disciplinas: localStorage.getItem('disciplinas')
-        ? JSON.parse(localStorage.getItem('disciplinas'))
-        : { '': 0 }
+    series:
+        localStorage.getItem('series') !== null
+            ? JSON.parse(localStorage.getItem('series'))
+            : { '': 0 },
+    disciplinas:
+        localStorage.getItem('disciplinas') !== null
+            ? JSON.parse(localStorage.getItem('disciplinas'))
+            : { '': 0 }
 };
 
 const reducer = (state = initialState, action) => {
