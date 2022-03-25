cbc73d14defcc88d060117a73b0220bbafd12eb4
Anderson Ribeiro Lopes <zeander21@outlook.com.br>
2022-03-25 10:12:40 -0300

    Home Page front-end fixes

--- resources/js/components/pages/_home/HomePage.js
+++ resources/js/components/pages/_home/HomePage.js
@@ -1,20 +1,9 @@
 import React, { useEffect } from 'react';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import {
-    CssBaseline,
-    Container,
-    Grid,
-    Button,
-    Card,
-    IconButton
-} from '@mui/material';
-import Copyright from '../../layout/Copyright';
+import { Grid, Button, Card, IconButton } from '@mui/material';
 import { bindActionCreators } from 'redux';
 import { useDispatch } from 'react-redux';
 import { actionCreators } from '../../../store/actionCreators';
 import { useNavigate } from 'react-router-dom';
-
-const theme = createTheme();
 
 export default function HomePage({}) {
     const navigate = useNavigate();
@@ -37,73 +26,64 @@ export default function HomePage({}) {
         }, 2000);
     }, []);
     return (
-        <ThemeProvider theme={theme}>
-            <Container
-                component="main"
-                sx={{
-                    marginTop: 10
-                }}
+        <>
+            <Grid
+                container
+                align="center"
+                alignItems="flex-start"
+                justifyContent="center"
+                spacing={3}
             >
-                <CssBaseline />
-                <Grid
-                    container
-                    align="center"
-                    alignItems="flex-start"
-                    justifyContent="center"
-                    spacing={3}
-                >
-                    {game_types.map((type, index) => {
-                        return (
-                            <Grid key={index} item xs={6} md={4} lg={4}>
-                                <Grid container align="center" spacing={2}>
-                                    <Grid item xs={12}>
-                                        <IconButton
-                                            disableRipple={true}
-                                            onClick={() =>
-                                                navigate(`/create/${type.slug}`)
-                                            }
-                                        >
-                                            <Card
-                                                elevation={5}
-                                                sx={{
-                                                    width: 310,
-                                                    height: 200,
-                                                    borderRadius: 4.5
-                                                }}
-                                            >
-                                                <img
-                                                    src={`/storage/games/${type.slug}.png`}
-                                                    alt={type.name}
-                                                    width="100%"
-                                                    height="100%"
-                                                />
-                                            </Card>
-                                        </IconButton>
-                                    </Grid>
-                                    <Grid
-                                        item
-                                        xs={12}
-                                        align="center"
-                                        sx={{
-                                            marginTop: 2
-                                        }}
+                {game_types.map((type, index) => {
+                    return (
+                        <Grid key={index} item xs={6} md={4} lg={4}>
+                            <Grid container align="center" spacing={2}>
+                                <Grid item xs={12}>
+                                    <IconButton
+                                        disableRipple={true}
+                                        onClick={() =>
+                                            navigate(`/create/${type.slug}`)
+                                        }
                                     >
-                                        <Button
-                                            onClick={() =>
-                                                navigate(`/create/${type.slug}`)
-                                            }
-                                            variant="outlined"
+                                        <Card
+                                            elevation={5}
+                                            sx={{
+                                                width: 310,
+                                                height: 200,
+                                                borderRadius: 4.5
+                                            }}
                                         >
-                                            {type.name}
-                                        </Button>
-                                    </Grid>
+                                            <img
+                                                src={`/storage/games/${type.slug}.png`}
+                                                alt={type.name}
+                                                width="100%"
+                                                height="100%"
+                                            />
+                                        </Card>
+                                    </IconButton>
+                                </Grid>
+                                <Grid
+                                    item
+                                    xs={12}
+                                    align="center"
+                                    sx={{
+                                        marginTop: 2
+                                    }}
+                                >
+                                    <Button
+                                        onClick={() =>
+                                            navigate(`/create/${type.slug}`)
+                                        }
+                                        variant="outlined"
+                                    >
+                                        {type.name}
+                                    </Button>
                                 </Grid>
                             </Grid>
-                        );
-                    })}
-                </Grid>
-            </Container>
-            <Copyright />
-        </ThemeProvider>
+                        </Grid>
+                    );
+                })}
+            </Grid>
+        </>
     );
 }
