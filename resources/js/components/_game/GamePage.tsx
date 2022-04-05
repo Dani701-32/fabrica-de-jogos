9cd32a72b8e3b5dbb9320fef87ca377ed9b7f89e
Anderson Ribeiro Lopes <zeander21@outlook.com.br>
2022-04-05 15:44:26 -0300

    Updated game event listener logic

--- resources/js/components/_game/GamePage.tsx
+++ resources/js/components/_game/GamePage.tsx
@@ -2,7 +2,8 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { setBaseState } from '../../reducers/baseReducer';
-import { createGameEventListener } from '../../actions/baseActions';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../store';
 
 type Props = {
     game: String;
@@ -11,13 +12,39 @@ type Props = {
 export default function GamePage({ game }: Props) {
     const { slug } = useParams();
     const [open, setOpen] = useState(false);
+    const { token, origin } = useSelector((state: RootState) => state.base);
     const queryString = window.location.search;
     const urlParams = new URLSearchParams(queryString);
     const dispatch = useDispatch();
     useEffect(() => {
         dispatch(setBaseState());
         setTimeout(() => {
-            dispatch(createGameEventListener(slug as string, urlParams));
+            window.addEventListener('message', (event) => {
+                if (event.origin !== window.location.origin) {
+                    const data = event.data;
+                    if (data.loaded) {
+                        let game_address =
+                            'https://fabricadejogos.portaleducacional.tec.br';
+                        let iframe: HTMLIFrameElement = document.getElementById(
+                            'frame'
+                        ) as HTMLIFrameElement;
+                        const message = JSON.stringify({
+                            user_token: token,
+                            origin: origin,
+                            game_address: game_address,
+                            slug: slug,
+                            aula_id: urlParams.has('aula_id')
+                                ? urlParams.get('aula_id')
+                                : 0,
+                            conteudo_id: urlParams.has('conteudo_id')
+                                ? urlParams.get('conteudo_id')
+                                : 0
+                        });
+                        // @ts-ignore
+                        iframe.contentWindow.postMessage(message, '*');
+                    }
+                }
+            });
             setOpen(true);
         }, 1000);
     }, [localStorage.getItem('token')]);
