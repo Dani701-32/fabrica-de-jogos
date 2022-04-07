2d7c8017c7ea9ed129b21e22d113df6dcbb55946
Anderson Ribeiro Lopes <zeander21@outlook.com.br>
2022-04-07 10:56:31 -0300

    User info nav bar pre-implementation

--- resources/js/reducers/userReducer.ts
+++ resources/js/reducers/userReducer.ts
@@ -2,16 +2,11 @@ import { createSlice } from '@reduxjs/toolkit';
 import { userState } from '../types';
 
 const initialState: userState = {
-    name: localStorage.getItem('name') ?? 'Gerson Rodriguez Barbacena',
-    role: localStorage.getItem('role') ?? 'Professor',
-    pfp:
-        localStorage.getItem('pfp') ??
-        'https://c.tenor.com/_rbT42ogn6MAAAAC/nagatoro-pfp.gif',
-    prefeitura_nome:
-        localStorage.getItem('prefeitura') ?? 'Prefeitura de São Paulo',
-    prefeitura_logo:
-        localStorage.getItem('logo') ??
-        'https://sp156.s3-sa-east-1.amazonaws.com/logo_cidadeSP.png',
+    name: localStorage.getItem('name') ?? '',
+    role: localStorage.getItem('role') ?? '',
+    pfp: localStorage.getItem('pfp') ?? '',
+    prefeitura_nome: localStorage.getItem('prefeitura') ?? '',
+    prefeitura_logo: localStorage.getItem('logo') ?? '',
     token: localStorage.getItem('token'),
     api_address: localStorage.getItem('api_address'),
     origin: localStorage.getItem('origin'),
