4155d21b96945f9a88702f8fb2a3db11ef73fa74
Anderson Ribeiro Lopes <zeander21@outlook.com.br>
2022-04-08 14:34:03 -0300

    Update nav bar

--- resources/js/components/_layout/NavBar.tsx
+++ resources/js/components/_layout/NavBar.tsx
@@ -18,67 +18,68 @@ const NavBar = () => {
     return (
         <>
             <AppBar position="static">
+                {/*
+                    <Container maxWidth="xl">
+                        <Toolbar disableGutters>
+                            <Grid sx={{ margin: 2 }}>
+                                <img
+                                    src="{data?.data.prefeitura_logo}"
+                                    alt="logo"
+                                    height="50px"
+                                />
+                            </Grid>
+                            <Typography
+                                variant="h6"
+                                noWrap
+                                sx={{
+                                    mr: 2,
+                                    display: { xs: 'none', md: 'flex' }
+                                }}
+                            >
+                                andrinor
+                            </Typography>
+                        </Toolbar>
+                    </Container>
+                */}
                 <Container maxWidth="xl">
                     <Toolbar disableGutters>
-                        <Grid sx={{ margin: 2 }}>
-                            <img
-                                src="{data?.data.prefeitura_logo}"
-                                alt="logo"
-                                height="50px"
+                        <Box sx={{ flexGrow: 0 }}>
+                            <Avatar
+                                alt="avatar"
+                                src={`https://metech.s3.flexify.io/${data?.data.pfp}`}
+                                sx={{ p: 0, margin: 2, height: 50, width: 50 }}
                             />
+                        </Box>
+                        <Grid container direction="column">
+                            <Grid item xs={9}>
+                                <Typography
+                                    variant="h6"
+                                    noWrap
+                                    sx={{
+                                        mr: 2,
+                                        display: { xs: 'none', md: 'flex' }
+                                    }}
+                                >
+                                    {data?.data.name}
+                                </Typography>
+                            </Grid>
+                            <Grid item xs={3}>
+                                <Typography
+                                    variant="subtitle2"
+                                    color="primary"
+                                    noWrap
+                                    sx={{
+                                        mr: 2,
+                                        display: { xs: 'none', md: 'flex' }
+                                    }}
+                                >
+                                    {data?.data.role}
+                                </Typography>
+                            </Grid>
                         </Grid>
-                        <Typography
-                            variant="h6"
-                            noWrap
-                            sx={{
-                                mr: 2,
-                                display: { xs: 'none', md: 'flex' }
-                            }}
-                        >
-                            andrinor
-                        </Typography>
                     </Toolbar>
                 </Container>
             </AppBar>
-            <Container maxWidth="xl">
-                <Toolbar disableGutters>
-                    <Box sx={{ flexGrow: 0 }}>
-                        <Avatar
-                            alt="avatar"
-                            src={`https://metech.s3.flexify.io/${data?.data.pfp}`}
-                            sx={{ p: 0, margin: 2, height: 50, width: 50 }}
-                        />
-                    </Box>
-                    <Grid container direction="column">
-                        <Grid item xs={9}>
-                            <Typography
-                                variant="h6"
-                                color="primary"
-                                noWrap
-                                sx={{
-                                    mr: 2,
-                                    display: { xs: 'none', md: 'flex' }
-                                }}
-                            >
-                                {data?.data.name}
-                            </Typography>
-                        </Grid>
-                        <Grid item xs={3}>
-                            <Typography
-                                variant="subtitle2"
-                                color="primary"
-                                noWrap
-                                sx={{
-                                    mr: 2,
-                                    display: { xs: 'none', md: 'flex' }
-                                }}
-                            >
-                                {data?.data.role}
-                            </Typography>
-                        </Grid>
-                    </Grid>
-                </Toolbar>
-            </Container>
         </>
     );
 };
