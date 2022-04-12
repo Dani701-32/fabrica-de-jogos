077b4d385d2f862faa0e5eae43a9400ecf0ef467
Anderson Ribeiro Lopes <zeander21@outlook.com.br>
2022-04-12 13:00:00 -0300

    user reducer wrong ""

--- resources/js/reducers/userReducer.ts
+++ resources/js/reducers/userReducer.ts
@@ -7,8 +7,8 @@ const initialState: userState = {
     token: localStorage.getItem('token'),
     api_address: localStorage.getItem('api_address'),
     origin: localStorage.getItem('origin'),
-    series: JSON.parse(localStorage.getItem('series') ?? "{ '': 0 }"),
-    disciplinas: JSON.parse(localStorage.getItem('disciplinas') ?? "{ '': 0 }")
+    series: JSON.parse(localStorage.getItem('series') ?? '{ "": 0 }'),
+    disciplinas: JSON.parse(localStorage.getItem('disciplinas') ?? '{ "": 0 }')
 };
 
 export const baseSlice = createSlice({
