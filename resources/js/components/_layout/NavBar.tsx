cbec141cc4287c3b248852181815eeba07af1f81
Anderson Ribeiro Lopes <zeander21@outlook.com.br>
2022-04-14 10:48:25 -0300

    Front-end changes in nav-bar

--- resources/js/components/_layout/NavBar.tsx
+++ resources/js/components/_layout/NavBar.tsx
@@ -3,6 +3,7 @@ import {
     AppBar,
     Avatar,
     Box,
+    CircularProgress,
     Container,
     Grid,
     Toolbar,
@@ -13,21 +14,51 @@ import { RootState } from '../../store';
 import { useGetUserInfoQuery } from '../../services/portal';
 
 const NavBar = () => {
-    const token = useSelector((state: RootState) => state.user.token);
-    const { data, isLoading } = useGetUserInfoQuery(token as string);
+    const { token, origin } = useSelector((state: RootState) => state.user);
+    const { data, isLoading } = useGetUserInfoQuery({ token, origin });
+    if (isLoading)
+        return (
+            // @ts-ignore
+            <Grid item align="center">
+                <CircularProgress />
+            </Grid>
+        );
     return (
         <>
             <AppBar position="static">
-                {/*
-                    <Container maxWidth="xl">
-                        <Toolbar disableGutters>
-                            <Grid sx={{ margin: 2 }}>
-                                <img
-                                    src="{data?.data.prefeitura_logo}"
-                                    alt="logo"
-                                    height="50px"
-                                />
-                            </Grid>
+                <Container maxWidth="xl">
+                    <Toolbar disableGutters>
+                        <Grid sx={{ margin: 2 }}>
+                            <img
+                                src={data?.data.prefeitura_logo}
+                                alt="logo"
+                                height="50px"
+                            />
+                        </Grid>
+                        <Typography
+                            variant="h6"
+                            noWrap
+                            sx={{
+                                mr: 2,
+                                display: { xs: 'none', md: 'flex' }
+                            }}
+                        >
+                            {data?.data.prefeitura_nome}
+                        </Typography>
+                    </Toolbar>
+                </Container>
+            </AppBar>
+            <Container maxWidth="xl">
+                <Toolbar disableGutters>
+                    <Box sx={{ flexGrow: 0 }}>
+                        <Avatar
+                            alt="avatar"
+                            src={`https://metech.s3.flexify.io/${data?.data.pfp}`}
+                            sx={{ p: 0, margin: 2, height: 50, width: 50 }}
+                        />
+                    </Box>
+                    <Grid container direction="column">
+                        <Grid item xs={9}>
                             <Typography
                                 variant="h6"
                                 noWrap
@@ -36,50 +67,25 @@ const NavBar = () => {
                                     display: { xs: 'none', md: 'flex' }
                                 }}
                             >
-                                andrinor
+                                {data?.data.name}
                             </Typography>
-                        </Toolbar>
-                    </Container>
-                */}
-                <Container maxWidth="xl">
-                    <Toolbar disableGutters>
-                        <Box sx={{ flexGrow: 0 }}>
-                            <Avatar
-                                alt="avatar"
-                                src={`https://metech.s3.flexify.io/${data?.data.pfp}`}
-                                sx={{ p: 0, margin: 2, height: 50, width: 50 }}
-                            />
-                        </Box>
-                        <Grid container direction="column">
-                            <Grid item xs={9}>
-                                <Typography
-                                    variant="h6"
-                                    noWrap
-                                    sx={{
-                                        mr: 2,
-                                        display: { xs: 'none', md: 'flex' }
-                                    }}
-                                >
-                                    {data?.data.name}
-                                </Typography>
-                            </Grid>
-                            <Grid item xs={3}>
-                                <Typography
-                                    variant="subtitle2"
-                                    color="primary"
-                                    noWrap
-                                    sx={{
-                                        mr: 2,
-                                        display: { xs: 'none', md: 'flex' }
-                                    }}
-                                >
-                                    {data?.data.role}
-                                </Typography>
-                            </Grid>
                         </Grid>
-                    </Toolbar>
-                </Container>
-            </AppBar>
+                        <Grid item xs={3}>
+                            <Typography
+                                variant="subtitle2"
+                                color="primary"
+                                noWrap
+                                sx={{
+                                    mr: 2,
+                                    display: { xs: 'none', md: 'flex' }
+                                }}
+                            >
+                                {data?.data.role}
+                            </Typography>
+                        </Grid>
+                    </Grid>
+                </Toolbar>
+            </Container>
         </>
     );
 };
